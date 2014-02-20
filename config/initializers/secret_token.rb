3eadf0715691d38a839354b872d16fa672353166
root <neginsky58@gmail.com>
2014-02-20 02:10:00 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Likeresponder::Application.config.secret_key_base = 'c9e8441737a2ce17fc009e11cea17b2b23352fc89e0dfa183acbb2a747cedd585d6af0158835b601daec01b4b8f77ab4d9a8bf7d78781fb82dff03f6da447c96'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Likeresponder::Application.config.session_store :cookie_store, key: '_likeresponder_session'
