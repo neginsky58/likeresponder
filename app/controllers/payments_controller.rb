c7dedb78fa0e0e8db8b755d294ae66a04081102d
root <neginsky58@gmail.com>
2014-02-27 03:09:17 +0200

    working

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,7 +1,13 @@
 class PaymentsController < ApplicationController
   
   def monthly_plan
-    @contact = Infusionsoft.data_load('Contact', 12, [:FirstName, :LastName])
+    
+    @contact = Infusionsoft.data_load('Contact', 190, [:FirstName, :LastName])
+    #Infusionsoft.contact_update(190,{ :FirstName => 'Yevgeny', :Email => 'yevgeny@test.com', :_StripeID =>'stripeid test' })
+    #@contact = Infusionsoft.data_load('Contact', 190, [:FirstName, :LastName, :StripeID])
+    @customfield1 = Infusionsoft.data_query('Contact', 1, 0, {:Id=>190}, [:_StripID])
+    Infusionsoft.data_update_custom_field(190, {:_StripeID => 123123})
+    @customfield2 = Infusionsoft.data_query('Contact', 1, 0, {:Id=>190}, [:_StripID])
   end
   
   def quarterly_plan
