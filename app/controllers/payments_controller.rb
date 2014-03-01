282fe891ca8f15078e711cc82e2226ef14440bf3
root <neginsky58@gmail.com>
2014-03-01 18:12:38 +0200

    fixing

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,21 +1,29 @@
 class PaymentsController < ApplicationController
   
   def monthly_plan
+
+    @contact = Infusionsoft.contact_add({:FirstName=>'Yev2', :LastName=>'Neg2', :_StripeID =>'cus_2123ADS12312'})
+    @contact = Infusionsoft.contact_load(@contact.to_i, [:FirstName, :LastName, :_StripeID])
     
-    @contact = Infusionsoft.data_load('Contact', 190, [:FirstName, :LastName])
-    #Infusionsoft.contact_update(190,{ :FirstName => 'Yevgeny', :Email => 'yevgeny@test.com', :_StripeID =>'stripeid test' })
-    #@contact = Infusionsoft.data_load('Contact', 190, [:FirstName, :LastName, :StripeID])
-    @customfield1 = Infusionsoft.data_query('Contact', 1, 0, {:Id=>190}, [:_StripID])
-    Infusionsoft.data_update_custom_field(190, {:_StripeID => 123123})
-    @customfield2 = Infusionsoft.data_query('Contact', 1, 0, {:Id=>190}, [:_StripID])
   end
   
   def quarterly_plan
   end
 
   def annual_plan
+
+    @contact = Infusionsoft.data_load('Contact', 190, [:FirstName, :LastName])
+    #Infusionsoft.contact_update(190,{ :FirstName => 'Yevgeny', :Email => 'yevgeny@test.com', :_StripeID =>'stripeid test' })
+    #@contact = Infusionsoft.data_load('Contact', 190, [:FirstName, :LastName, :StripeID])
+    @customfield1 = Infusionsoft.data_query('Contact', 1, 0, {:Id=>190}, [:_StripID])
+    #Infusionsoft.data_update_custom_field('_StripeID', {'190'  => '123123'})
+    @customfield2 = Infusionsoft.data_query('DataFormField', 1, 0, {:Values=>'Test stripe id'}, [:Id])
+
   end
 
   def update_creditcard
   end
+  def index
+  end
+  
 end
